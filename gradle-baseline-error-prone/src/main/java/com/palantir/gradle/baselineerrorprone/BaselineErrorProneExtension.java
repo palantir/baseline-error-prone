b8217dcceb873574249b0a8f8048181e0348a8be
Kelvin Ou <58218998+kelvinou01@users.noreply.github.com>
2025-12-05 13:11:57 +0000

    Move Gradle plugins to `com.palantir.gradle.baselineerrorprone` (#14)
    Move Gradle plugins to `com.palantir.gradle.baselineerrorprone`

--- gradle-baseline-error-prone/src/main/java/com/palantir/gradle/baselineerrorprone/BaselineErrorProneExtension.java
+++ gradle-baseline-error-prone/src/main/java/com/palantir/gradle/baselineerrorprone/BaselineErrorProneExtension.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2025 Palantir Technologies Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.palantir.baseline;
+package com.palantir.gradle.baselineerrorprone;
 
 import com.palantir.gradle.suppressibleerrorprone.SuppressibleErrorProneExtension;
 import java.util.Set;
